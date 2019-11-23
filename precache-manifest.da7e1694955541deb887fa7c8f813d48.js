2e96398a28b08c094fddcfd695dcc5af1ecf1b44
FrankieFigz <fjvannini@gmail.com>
2019-11-23 17:56:10 -0500

    Updates

---
+++ precache-manifest.da7e1694955541deb887fa7c8f813d48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5024ee578e99c92b7f247d2624ede76",
+    "url": "/deployed-click-me/index.html"
+  },
+  {
+    "revision": "6bf7399802759f0287ff",
+    "url": "/deployed-click-me/static/css/main.e668aeb4.chunk.css"
+  },
+  {
+    "revision": "c31e1109d0b4b061aa71",
+    "url": "/deployed-click-me/static/js/2.19f41e55.chunk.js"
+  },
+  {
+    "revision": "6bf7399802759f0287ff",
+    "url": "/deployed-click-me/static/js/main.bd5f6479.chunk.js"
+  },
+  {
+    "revision": "5eb94e5fa8dc5f94114a",
+    "url": "/deployed-click-me/static/js/runtime-main.6a01408e.js"
+  }
+]);
